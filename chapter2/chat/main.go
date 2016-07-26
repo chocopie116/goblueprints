306ea1497511e1f7325e606e41415a2b95b18620
chocopie116 <chocopie116@gmail.com>
2016-07-26 21:44:03 +0900

    Add redirect auth provider url

--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -7,6 +7,9 @@ import (
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
 )
 
 type templateHandler struct {
@@ -25,6 +28,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "app listen port")
 	flag.Parse()
+	gomniauth.SetSecurityKey("majideeeee")
+	gomniauth.WithProviders(
+		facebook.New("1144919502229998", "c8f962947be4774c2d91cf53e5f9e876", "http://localhost:8080/auth/callback/facebook"),
+	)
+
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
